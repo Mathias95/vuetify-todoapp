82cb7735886d00a497a0d17678fed52902b968aa
Mathias95 <mathiasrjacobsen@gmail.com>
2019-01-09 22:17:21 +0100

    side drawer added on frontpage

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
         <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>Mathias</span>
@@ -11,12 +12,20 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer app class="indigo" v-model="drawer">
+            test
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            drawer: false
+        }
+    }
 }
 </script>
 
